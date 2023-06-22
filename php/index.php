d455dfe1a52bcbce3eb642181c2d80aeef64ec60
Ninotro <ninotroia96@gmail.com>
2023-06-22 13:50:43 +0200

    first commit

---
+++ php/index.php
@@ -0,0 +1,8 @@
+<?php
+
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Headers: X-Requested-With");
+header('Content-Type: application/json');
+	
+$studentStr = file_get_contents('data.json');
+echo $studentStr;
